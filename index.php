f546c8a5f20d86dd0309c2a163a9c4533114a564
Riley <riley.childs@yahoo.com>
2021-09-07 19:19:34 -0400

    Changing image

--- index.php
+++ index.php
@@ -4,10 +4,19 @@
 <main>
 
 <h1>Your new dream home awaits...</h1>
+<p id="test">TEST</p>
 </main>
 
 
-
+<script>
+    //Use promises
+ window.setInterval("changeImage()", 3000);
+ let images=["1", "2", "3", "4"];
+ let changeImage = () =>{
+    document.getElementById("test").innerHTML = images[Math.floor(Math.random() * images.length)];
+ 
+}
+</script>
 <?php include 'include/footer.php'; ?>
 
 </body>
