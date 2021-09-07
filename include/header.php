feb983a2d09183f8e5e23756990538dee54246dc
Riley <riley.childs@yahoo.com>
2021-09-07 17:23:41 -0400

    9-7

---
+++ include/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Childs Realty - <?php echo $pageTitle; ?></title>
+</head>
+<body>
+
+<div id="flex-top">
+<header>
+  <h1>Childs</h1>
+  <h4>Realty</h4>
+</header>
+
+<?php include 'include/navigation.php'; ?>
+</div>
---
+++ include/navigation.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Listings</li>
+        <li>Contact</li>
+    </ul>
+</nav>
