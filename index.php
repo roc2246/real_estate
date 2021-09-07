9215a324f543100c7751e67bcdd69455acace7bf
Riley <riley.childs@yahoo.com>
2021-09-07 17:23:41 -0400

    9-7

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php $pageTitle = 'Home'; ?>
+<?php include 'include/header.php';?>
+
+<main>
+
+<h1>Your new dream home awaits...</h1>
+</main>
+
+
+
+<?php include 'include/footer.php'; ?>
+
+</body>
+</html>
