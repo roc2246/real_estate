2d97a0ddc08124f8ab2984703f2b583ae61c92cc
Riley <riley.childs@yahoo.com>
2021-09-23 11:31:43 -0400

    Refactoring

--- include/navigation.php
+++ include/navigation.php
@@ -2,7 +2,7 @@
     <ul>
         <li><a href="index.php">Home</a></li>
         <li><a href="aboutus.php">About Us</a></li>
-        <li><a href="listings.php">listings</a></li>
+        <li><a href="listings.php">Listings</a></li>
         <li><a href="contact.php">Contact</a></li>
     </ul>
 </nav>
