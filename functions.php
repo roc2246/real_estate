41e13c1752395c880191976d54c5e5c76f913cc5
Riley <riley.childs@yahoo.com>
2021-10-01 09:51:54 -0400

    10-1-2021 progress on api

--- functions.php
+++ functions.php
@@ -6,18 +6,44 @@ function getapi($table){
     $sql = "SELECT * FROM $table";
     $response = array();
     $result = mysqli_query($connection, $sql);
-    if($result){
+      if($result){
         header("Content");
         $i = -1;
-        while($row = mysqli_fetch_assoc($result)){
+        while($row = mysqli_fetch_assoc($result) 
+        && $fieldinfo = mysqli_fetch_field($result)){
             $i++;
-        
-        array_push($response, $row['id'], $row['image'], $row['adress'], $row['price']);
+
+            $fieldName = printf("%s\n", $fieldinfo -> name);
+            //echo $fieldName;
+            //echo $fieldinfo;
+            $response[$fieldinfo[$i]] = $row['id'];  
+
+           // echo $fieldinfo[$i];
+            print_r(array_values($response));
+
+            //Place $response in this while loop?
+
+         //array_push($response, $row['id'], $row['image'], $row['adress'], $row['price']);
         //Insert more columns here
         }
-        echo json_encode($response, JSON_PRETTY_PRINT);
-    } 
+        //echo json_encode($response, JSON_PRETTY_PRINT); 
+
+    
+   /*  if ($result) {
+        // Get field information for all fields
+        while ($fieldinfo = mysqli_fetch_field($result)) {
+          printf("Name: %s\n", $fieldinfo -> name);
+          echo "<br>";
+          printf("Table: %s\n", $fieldinfo -> table);
+          echo "<br>";
+          printf("max. Len: %d\n", $fieldinfo -> max_length);
+          echo "<br>";
+        }
+        mysqli_free_result($result);
+      } */
+    }
 }
+
 
 function sendEmail($mailTo){
     if(isset($_POST['submit'])){
