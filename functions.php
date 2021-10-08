a16e509c7662009b3ab17718891bf30ea0ec3e4c
Riley <riley.childs@yahoo.com>
2021-10-08 11:04:28 -0400

    Deleted scrap code

--- functions.php
+++ functions.php
@@ -51,19 +51,7 @@ function enableUpload(){
        }
   }
 
-/* <form name="uploads"  method="post" autocomplete="off">
-       <label>Brand:</label>
-        <input type="text" name="brand"><br><br>
-       <label>Model:</label>
-        <input type="text" name="model"><br><br>
-       <label>Price:</label>
-        <input type="text" name="price"><br><br>
-       <label>Size:</label>
-        <input type="text" name="size"><br><br>
-       <label>Product Image:</label>
-       <?php enableUpload();?>
-<button type="submit" value="submit" name="submit">submit</button>
- 
+/* 
 <div id="concept">
 <h4>Upload Status</h4>
 <?php invenProd(); checkTempLocation(); ?>
