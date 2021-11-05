e46532860b1adeac002742440ac15863006d7ff1
Riley <riley.childs@yahoo.com>
2021-11-05 10:27:09 -0400

    Fixed a minor typo in the comments

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ global $connection;
       }
   }
 
-  //Prevents A new listing from being creaqted upon submission
+  //Prevents A new listing from being created upon submission
   if ( window.history.replaceState ) {
         window.history.replaceState( null, null, window.location.href );
     }
